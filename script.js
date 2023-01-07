055108649eaf44671fbd182e85f3b6f5fd276528
Елена <elena@MacBook-Air-Elena-2.local>
2023-01-07 14:00:15 +0300

    добавила второе задание

--- script.js
+++ script.js
@@ -18,11 +18,57 @@ class Worker {
     }
 }
 
-
 var worker = new Worker('Иван', 'Иванов', 10, 31);
 
-console.log(worker.name); //выведет 'Иван'
-console.log(worker.surname); //выведет 'Иванов'
-console.log(worker.rate); //выведет 10
-console.log(worker.days); //выведет 31
-console.log(worker.getSalary()); //выведет 310 - то есть 10*31
+console.log(worker.name);
+console.log(worker.surname);
+console.log(worker.rate);
+console.log(worker.days);
+console.log(worker.getSalary()); 
+
+
+// Помните наше практическое задание с опросником для владельцев котиков? Он нам снова пригодится! 
+// Напишите класс Cat со свойствами, соответствующими полям вашего опросника (например, кличка, корм и пр.), 
+// и по нажатию на кнопку "Ок" создайте экземпляр этого класса, заполнив его свойства ответами из опросника. 
+// В этом классе будут только свойства и конструктор, без методов. Выведите результат (заполненный объект класса Cat)
+//  в консоль при помощи console.log.
+
+const catName = document.querySelector("#catname");
+const catBreed = document.querySelector("#catbreed");
+// const catGender = document.querySelectorAll('input[name="sex"]');
+const saveCat = document.querySelector("#savecat");
+
+class Cat {
+    constructor (name, breed, gender) {
+        this.name = name;
+        this.breed = breed;
+        this.gender = gender;
+    }
+}
+
+let cat = {};
+
+saveCat.onclick = function () {
+cat.name = catName.value;
+cat.breed = catBreed.value;
+// cat.gender = catGender.value;
+
+const catGender = document.querySelectorAll('input[name="sex"]');
+for (let i=0; i<catGender.length; i++) {
+function getGender () {
+    if (catGender[i].checked) {
+        const gotGender = catGender[i].value;
+        return gotGender
+    }
+}}
+const fuckingGender = getGender();
+//console.log(fuckingGender);
+cat.gender = fuckingGender;
+
+console.log(cat);
+}
+    
+
+
+
+
